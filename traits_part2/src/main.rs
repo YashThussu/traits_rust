9aa6bccc5104cf2d9a24675f6943a317d9bf6cda
yashthussu <yashuthussu@gmail.com>
2025-08-15 03:13:05 +0530

    trait inheritance

--- traits_part2/src/main.rs
+++ traits_part2/src/main.rs
@@ -1,14 +1,21 @@
-trait Taxable {
+trait Investment {
 
-    const TAX_RATE: f64 = 0.25;
-
-    fn set_amount(&mut self, new_amount:f64); // setter method
+     fn set_amount(&mut self, new_amount:f64); // setter method
 
     fn double_amount(&mut self){
         self.set_amount(self.amount() * 2.0);
     } // 
 
     fn amount(&self)->f64; // setter method
+
+}
+
+//taxable traits inherit from Investement trait
+// this allows us to use the methods defined in Investement trait
+trait Taxable: Investment {
+
+    const TAX_RATE: f64 = 0.25;
+
 
     fn tax_bill(&self)->f64{
         self.amount()*Self::TAX_RATE
@@ -21,9 +28,8 @@ struct Income{
     amount: f64,
 }
 
-impl Taxable for Income{
-
-    fn amount(&self)->f64{
+impl Investment for Income {
+        fn amount(&self)->f64{
         self.amount
     }
 
@@ -32,14 +38,14 @@ impl Taxable for Income{
     }
 }
 
+impl Taxable for Income{}
+
 #[derive(Debug)]
 struct Bonus{
     value: f64,
 }
 
-impl Taxable for Bonus{
-
-    const TAX_RATE:f64=0.30;
+impl Investment for Bonus {
 
     fn set_amount(&mut self, new_amount:f64) {
         self.value = new_amount;
@@ -47,6 +53,26 @@ impl Taxable for Bonus{
     
     fn amount(&self)->f64{
         self.value
+    }
+}
+
+impl Taxable for Bonus{
+
+    const TAX_RATE:f64=0.30;
+}
+
+struct QualityTime {
+    minutes:f64,
+}
+
+impl Investment for QualityTime{
+
+    fn amount(&self)->f64{
+        self.minutes
+    }
+
+    fn set_amount(&mut self, new_amount:f64){
+        self.minutes = new_amount;
     }
 }
 
@@ -63,4 +89,10 @@ fn main() {
     println!("Tax rate for bonus: {:.2}", Bonus::TAX_RATE);
     bonus.double_amount();
     println!("New amount after doubling: {:.2}", bonus.amount());
+
+    let mut quality_time=QualityTime{minutes: 120.0};
+    println!("Relaxation time :{:.2}", quality_time.amount());
+    println!("Doubling relaxation time...");
+    quality_time.double_amount();
+    println!("New relaxation time: {:.2}", quality_time.amount());
 }
