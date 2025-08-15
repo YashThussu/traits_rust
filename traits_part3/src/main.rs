1cef146f35283359dc349b4ba89f144cd4fd291b
yashthussu <yashuthussu@gmail.com>
2025-08-16 02:53:12 +0530

    custom display traits involving enums

--- traits_part3/src/main.rs
+++ traits_part3/src/main.rs
@@ -1,7 +1,24 @@
 use std::fmt::{Display,Formatter,Result};
 
+enum AppleType {
+    GrannySmith,
+    Fuji,
+    Honeycrisp
+}
+
+impl Display for AppleType {
+
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+         write!(f,"{}",match self {
+             AppleType::GrannySmith=>"Granny Smith",
+             AppleType::Fuji=>"Fuji",
+             AppleType::Honeycrisp=>"Honeycrisp"
+         })
+    }
+}
+
 struct Apple {
-    kind:String,
+    kind:AppleType,
     price:f64
 }
 
@@ -14,9 +31,15 @@ impl Display for Apple {
 fn main() {
 
     let lunch_snack= Apple {
-        kind: String::from("Granny Smith"),
+        kind: AppleType::GrannySmith,
         price: 1.04
     };
 
+    let dinner= Apple{
+        kind: AppleType::Fuji,
+        price: 1.25
+    };
+
     println!("{}",lunch_snack);
+    println!("{}",dinner);
 }
